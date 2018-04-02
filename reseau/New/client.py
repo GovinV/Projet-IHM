16ba2b14441142595b3a410bbcc877ad752964c6
Govin <gvetrivel@etu.unistra.fr>
2018-04-02 12:30:21 +0200

    Server works like a chat but broadcast msg to player in a room, test with client.py in Reseau/New

---
+++ reseau/New/client.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
+import select, socket, sys
+from hall import Room, Hall
+import hall
+from player import Player
+import player
+
+READ_BUFFER = 4096
+host = "127.0.0.1"
+port = 8888 
+
+
+server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_connection.connect((host,port))
+
+def prompt():
+    print('>', end=' ', flush = True)
+
+print("Connected to server\n")
+msg_prefix = ''
+
+socket_list = [sys.stdin, server_connection]
+
+while True:
+    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+    for s in read_sockets:
+        if s is server_connection: # incoming message 
+            msg = s.recv(READ_BUFFER)
+            if not msg:
+                print("Server down!")
+                sys.exit(2)
+            else:
+                if msg == hall.QUIT_STRING.encode():
+                    sys.stdout.write('Bye\n')
+                    sys.exit(2)
+                else:
+                    sys.stdout.write(msg.decode())
+                    if 'Please tell us your name' in msg.decode():
+                        msg_prefix = 'name: ' # identifier for name
+                    else:
+                        msg_prefix = ''
+                    prompt()
+
+        else:
+            msg = msg_prefix + sys.stdin.readline()
+            server_connection.sendall(msg.encode())
---
+++ reseau/New/server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
+import signal
+import select, socket, sys
+from hall import Hall, Room
+from player import Player
+import player
+
+
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+READ_BUFFER = 4096
+
+host = "127.0.0.1"
+port = 8888 
+MAX_CLIENTS = 30
+
+listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_sock.setblocking(0)
+listen_sock.bind((host,port))
+listen_sock.listen(MAX_CLIENTS)
+print("Now listening at ", (host, port))
+hall = Hall()
+connection_list = []
+connection_list.append(listen_sock)
+
+while True:
+    # Player.fileno()
+    read_players, write_players, error_sockets = select.select(connection_list, [], [])
+    for player in read_players:
+        if player is listen_sock: # new connection, player is a socket
+            new_socket, add = player.accept()
+            new_player = Player(new_socket)
+            connection_list.append(new_player)
+            hall.welcome_new(new_player)
+
+        else: # new message
+            msg = player.socket.recv(READ_BUFFER)
+            if msg:
+                msg = msg.decode().lower()
+                hall.handle_msg(player, msg)
+            else:
+                player.socket.close()
+                connection_list.remove(player)
+
+    for sock in error_sockets: # close error sockets
+        sock.close()
+        connection_list.remove(sock)
