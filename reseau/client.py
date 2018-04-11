9ff520ade11f70773913cfe428b2b386466a922b
Govin <gvetrivel@etu.unistra.fr>
2018-04-11 11:37:41 +0200

    client test and server modification

--- reseau/client.py
+++ reseau/client.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
 
-import socket
-import sys
+import select, socket, sys
+from lobby import Room, Lobby
+import lobby
+from player import Player
+import player
 
-def main():
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = "127.0.0.1"
-    port = 8888
+READ_BUFFER = 4096
+host = "127.0.0.1"
+port = 8888 
 
-    try:
-        soc.connect((host, port))
-    except:
-        print("Connection error")
-        sys.exit()
 
-    print("Enter 'quit' to exit")
-    message = input(" -> ")
+server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_connection.connect((host,port))
 
-    while message != 'quit':
-        soc.sendall(message.encode("utf8"))
-        if soc.recv(5120).decode("utf8") == "-":
-            pass        # continue
+def prompt():
+    print('>', end=' ', flush = True)
 
-        message = input(" -> ")
+print("Connected to server\n")
+msg_prefix = ''
 
-    soc.send(b'--quit--')
-    #soc.close()
+socket_list = [sys.stdin, server_connection]
 
-if __name__ == "__main__":
-    main()
+while True:
+    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+    for s in read_sockets:
+        if s is server_connection: # incoming message 
+            msg = s.recv(READ_BUFFER)
+            if not msg:
+                print("Server down!")
+                sys.exit(2)
+            else:
+                if msg == lobby.QUIT_STRING.encode():
+                    sys.stdout.write('Bye\n')
+                    sys.exit(2)
+                else:
+                    sys.stdout.write(msg.decode())
+                    if '<name>' in msg.decode():
+                        msg_prefix = 'name: ' # identifier for name
+                    else:
+                        msg_prefix = ''
+                    prompt()
+
+        else:
+            msg = msg_prefix + sys.stdin.readline()
+            server_connection.sendall(msg.encode())
