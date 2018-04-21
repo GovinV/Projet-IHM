daebffd8b287a9ff9097e58be763adc9b100eff9
NathanRth <nathan.roth67@yahoo.fr>
2018-04-21 22:36:17 +0200

    essai d'implementation de la gameloop [QEventLoop bug], implementation carte courante

--- uno/ui/jeu.h
+++ uno/ui/jeu.h
@@ -6,6 +6,7 @@
 #include <QQmlApplicationEngine>
 #include <QQmlComponent>
 #include <QEventLoop>
+#include <QDebug>
 
 #include "core/partie.h"
 #include "core/pioche.h"
@@ -16,12 +17,18 @@
 
 class Jeu : public QObject
 {
+    // read only
+    Q_PROPERTY(QString curCardColor READ curCardColor NOTIFY curCardColorChanged)
+    Q_PROPERTY(int curCardType READ curCardType NOTIFY curCardTypeChanged)
+    Q_PROPERTY(int curCardValue READ curCardValue NOTIFY curCardValueChanged)
     Q_OBJECT
-    Q_PROPERTY(int comp READ comp WRITE setComp NOTIFY compChanged)
 public:
     explicit Jeu(QObject *parent = nullptr);
 
-    // the 4 hands of the 4 players
+    /**
+     * @brief hands les 4 mains de 4 joueurs, permettent de remplir la vue QML
+     * doivent être remplis avec joueur.cmain
+     */
     Hand hands[4];
 
     //action triggered from QML
@@ -38,15 +45,19 @@ public:
     Q_INVOKABLE void piocher(int i);
 
     /**
-     * @brief player plays card
+     * @brief Le joueur joue une carte, utilise une fonction du noyau
+     * Exemple de prog evenementiel qui marche
      * @param i indice de la carte
      */
     Q_INVOKABLE void playCard(int i);
 
+    /**
+      * L'idée avec cette fonction c'est de reprendre la boucle initialement écrite
+      * dans le main du noyau. Comme on ne peux pas attendre une simple entrée clavier,
+      * on doit attendre une input depuis QML
+      * pour remplacer, on se sert de waitForBtPressed qui retourne le code boutton
+      */
     Q_INVOKABLE void gameLoop();
-
-    int comp(){return _comp;}
-    void setComp(int i){_comp = i;}
 
     /**
      * @brief bind les boutons QML aux attributs correspondant de la classe
@@ -56,19 +67,41 @@ public:
 
     /**
      * @brief Mets à jour le jeu (QML) du joueur
+     * Quand les cartes dans le noyau changent, Jeu:: a aucun moyen de la savoir
+     * Il faut donc trigger une fonction  pour update la vue
      * @param id_joueur l'id du joueur
      */
     void updateHand(int id_joueur);
 
+    Q_INVOKABLE void test();
 
+    /**
+     * @brief déclenche une QEvenLoop qui peut être libérée par un signal bt
+     * @return le code du boutton pressé
+     */
+    int waitForBtPressed();
+
+    // curent card values getter
+    QString curCardColor();
+    int curCardType();
+    int curCardValue();
+    void updateCurCard();
 
 signals:
+    // current card values change notif
+    void curCardColorChanged();
+    void curCardTypeChanged();
+    void curCardValueChanged();
 
-    void compChanged();
-
-    void cardPlayed();
 public slots:
+
+    // slots executés lorsque le bouton xBt est pressé
+
     void unoBtPressed();
+    void contreUnoBtPressed();
+    void playCardBtPressed(int i); // i porte l'indice de la carte active
+    void drawCardBtPressed();
+
 private:
 
     //fonctions de transition entre la classe et le core
@@ -90,9 +123,11 @@ private:
 
     Partie *m_partie;
 
-    QObject *unoBt, *drawCardBt, *playCardBt;
+    QObject *unoBt, *contreUnoBt, *drawCardBt, *playCardBt;
 
-    int _comp;
+    QEventLoop eventLoop;
+    int current_card_nb ;
+    int btPressed;
 };
 
 #endif // JEU_H
